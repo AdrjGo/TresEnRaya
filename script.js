ceec373ca1ae8ae352289d86e82647c53d5efd80
AdrjGo <gcadrianj@gmail.com>
2024-05-30 19:32:11 -0400

    Update victory

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let playerText = document.getElementById("text");
+let restartBtn = document.getElementById("restartBtn");
+let boxes = Array.from(document.getElementsByClassName("box"));
+
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
+  "--winning-blocks"
+);
+
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+
+const startGame = () => {
+  boxes.forEach((box) => box.addEventListener("click", boxClicked));
+};
+
 function boxClicked(e) {
   const id = e.target.id;
 
@@ -5,7 +22,7 @@ function boxClicked(e) {
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
 
-    const winner = playerHasWon();
+    const winner = playerWin();
     if (winner) {
       playerText.innerHTML = `${currentPlayer} GANASTE!`;
       highlightWin(winner);
@@ -27,15 +44,12 @@ function highlightWin(winningBlocks) {
 }
 
 const winCombo = [
-  //Filas
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
-  //Columnas
   [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  //Diagonales
   [0, 4, 8],
   [2, 4, 6],
 ];
@@ -50,3 +64,5 @@ function playerWin() {
   }
   return false;
 }
+
+startGame();
