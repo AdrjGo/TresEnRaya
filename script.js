5c58c0bab9702fd6bf523d1b405dbd69aa3e6bcb
Darknight108 <daniel.mancilla.lopez.015@gmail.com>
2024-05-30 18:38:50 -0400

    Add recover items

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const X_TEXT = 'X';
+const O_TEXT = 'O';
+const winnerIndicator = 'lightgreen';
+
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+const playerText = document.getElementById('playerText');
+const boxes = Array.from(docuemnt.getElementsByClassName('cell'));
+
+function startGame(){
+    currentPlayer = X_TEXT;
+    spaces =  Array(9).fill(null);
+    playerText.innerHTML = 'Turno de ${currentPlayer}';
+
+    boxes.forEach((box, index)=>{
+        box.innerText = '';
+        box.style.backgroundColor = '';
+        box.removeEventListener('click', boxCliend);
+        box.addEventListener('click', boxClicked,{ once: true});
+    });
+}
+
+
+
 function boxClicked(e) {
     const id = e.target.id;
 
@@ -18,8 +42,10 @@ function boxClicked(e) {
 
 function switchPlayer() {
     currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT;
+    playerText.innerHTML = 'Turno de ${currentPlayer}';
 }
 
 function highlightWin(winningBlocks) {
     winningBlocks.forEach(box => boxes[box].style.backgroundColor = winnerIndicator);
 }
+
