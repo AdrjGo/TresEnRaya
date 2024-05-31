309d8795f24a7ce4f4a4232f64b27811bf922a5a
AdrjGo <gcadrianj@gmail.com>
2024-05-30 22:08:11 -0400

    Update script

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let playerText = document.getElementById("text");
 let restartBtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box"));
 
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
-  "--winning-blocks"
-);
-
 const O_TEXT = "O";
 const X_TEXT = "X";
 let currentPlayer = X_TEXT;
